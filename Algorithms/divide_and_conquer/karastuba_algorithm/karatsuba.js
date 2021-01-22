d38ef18fb4868bb3362d88a66aaa0766a2704da5
samuelmayna <samuelmayna@gmail.com>
2021-01-22 09:20:34 +0300

    minor modification

--- Algorithms/divide_and_conquer/karastuba_algorithm/karatsuba.js
+++ Algorithms/divide_and_conquer/karastuba_algorithm/karatsuba.js
@@ -1,13 +1,16 @@
 module.exports = (base, int1, int2) => {
-  return karatsubaHelper(int1, int2);
-  function karatsubaHelper(int1, int2) {
-    //if any of the numbers can not be split(i.e is a one digit number)
-    //we do the normal multiplication.
-    if (int1 < base || int2 < base) {
-      return multiply(int1, int2);
-    }
+  const num1 = parseInt(int1, base);
+  const num2 = parseInt(int2, base);
+  //the algorithm will return the answert to base 10
+  //hence need to convert it to the needed base.
+  return convertBase(karatsubaHelper(num1, num2), 10, base);
 
-    const max = Math.max(noOfDigitsIn(int1), noOfDigitsIn(int2));
+  function karatsubaHelper(int1, int2) {
+    const max = getMax(noOfDigitsIn(int1), noOfDigitsIn(int2));
+
+    const threshhold = 2 * base;
+
+    if (max <= 2 && getMax(int1, int2) <= threshhold) return int1 * int2;
 
     const dividerPower = integerDivision(max, 2);
 
@@ -17,53 +20,40 @@ module.exports = (base, int1, int2) => {
     const ac = karatsubaHelper(a, c);
     const bd = karatsubaHelper(b, d);
     const r = karatsubaHelper(a + b, c + d);
-
-    const result =
+    const adPlusBc = r - ac - bd;
+    return (
       ac * power(base, 2 * dividerPower) +
-      (r - ac - bd) * power(base, dividerPower) +
-      bd;
-    return convertFromBase1ToBase2(result, 10, base);
+      adPlusBc * power(base, dividerPower) +
+      bd
+    );
   }
-  function split(number, dividerPower) {
-    const noOfDigits = noOfDigitsIn(number);
 
-    //reject if the number is not finite or it has one digit
-    if (!isFinite(number) || noOfDigits < 2) {
+  function split(number, dividerPower) {
+    if (!isFinite(number)) {
       throw Error(`Cannot split number = ${number}`);
     }
-    const divider = power(base, dividerPower);
+    //if it is a single digit no need to perform division and modulo operations.
+    if (number < base) return [0, number];
 
+    const divider = power(base, dividerPower);
     const num1 = integerDivision(number, divider);
-    const num2 = modulo(number, divider);
-    console.log(num1, num2);
+    const num2 = number % divider;
     return [num1, num2];
   }
-
-  function multiply(num1, num2) {
-    const v1 = convertFromBase1ToBase2(num1, base, 10);
-    const v2 = convertFromBase1ToBase2(num2, base, 10);
-    return convertFromBase1ToBase2(v1 * v2, 10, base);
-  }
-  function integerDivision(number, divisor) {
-    const v1 = convertFromBase1ToBase2(number, base, 10);
-
-    const inter = convertFromBase1ToBase2(v1 / divisor, 10, base);
-    const result = Math.floor(inter);
-    return result;
-  }
-  function modulo(number, divisor) {
-    const v1 = convertFromBase1ToBase2(number, base, 10);
-    return convertFromBase1ToBase2(v1 % divisor, 10, base);
-  }
 };
+
+function convertBase(num, fromBase, toBase) {
+  return Number(parseInt(num, fromBase).toString(toBase));
+}
 
 function noOfDigitsIn(int) {
   return int.toString().length;
 }
-
-function convertFromBase1ToBase2(num, fromBase, toBase) {
-  const decimal = parseInt(num, fromBase).toString(toBase);
-  return Number(decimal);
+function getMax(a, b) {
+  return Math.max(a, b);
+}
+function integerDivision(number, divisor) {
+  return Math.floor(number / divisor);
 }
 function power(base, pow) {
   return Math.pow(base, pow);

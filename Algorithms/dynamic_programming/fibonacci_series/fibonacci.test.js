482401e382310a87541adaa88736d88ca84b8578
samuelmayna <samuelmayna@gmail.com>
2021-01-22 08:26:42 +0300

    add algorithm  to add fibonacci_series

---
+++ Algorithms/dynamic_programming/fibonacci_series/fibonacci.test.js
@@ -0,0 +1,15 @@
+const fibonacci = require("./fibonacci");
+describe("Fibonacci Series", () => {
+  describe("should return fibonacci(0) and fibonacci(1)", () => {
+    it("0", () => {
+      expect(fibonacci(0)).toBe(0);
+    });
+    it("1", () => {
+      expect(fibonacci(1)).toBe(1);
+    });
+  });
+  it("return for huge values", () => {
+    expect(fibonacci(20)).toBe(6765);
+    expect(fibonacci(60)).toBe(1548008755920);
+  });
+});

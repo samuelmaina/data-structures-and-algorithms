0c8d922f44ad2936c53016ca4aea9a33c9d6ec7b
samuelmayna <samuelmayna@gmail.com>
2023-01-19 14:21:36 +0300

    Aded  more regrous tests for testing sorting fuctions

--- Algorithms/testUtils.js
+++ Algorithms/testUtils.js
@@ -1,16 +1,46 @@
-exports.sortTest = func => {
+exports.sortTest = (func) => {
+  describe("Invalid Inputs", () => {
+    it("undefined", () => {
+      expect(func(undefined)).toBeUndefined();
+    });
+    it("null", () => {
+      expect(func(null)).toBeNull();
+    });
+  });
+  describe("Edge case", () => {
+    it("an empty array ", () => {
+      expect(func([])).toStrictEqual([]);
+    });
+  });
   describe("numerical sorting", () => {
+    it("when array has 1 element", () => {
+      expect(func([1])).toStrictEqual([1]);
+    });
+
+    describe("when array has 2 element", () => {
+      runTests([2, 1]);
+    });
     let nonRepeating = [10, 99, 45, 22, 23, 25];
     let repeating = [12, 30, 4, 78, 44, 12, 44];
     runTests(nonRepeating, repeating);
   });
 
   describe("string sorting", () => {
-    let nonRepeating = ["s9", "s2", "s4", "s1", "s20", "s3"];
-    let repeating = ["s1", "s2", "s4", "s1", "s20", "s2"];
+    it("when array has 1 element", () => {
+      expect(func("a")).toBe("a");
+    });
+
+    describe("when array has 2 element", () => {
+      it("should run when we have only 2 elem", () => {
+        expect(func("ba", (a, b) => a < b)).toStrictEqual("ab");
+      });
+    });
+    let nonRepeating = "azyxAb";
+    let repeating = "bcdefbA";
     runTests(nonRepeating, repeating);
   });
-  function runTests(nonRepeating, repeating) {
+
+  function runTests(nonRepeating, repeating = []) {
     describe("ascending", () => {
       const ascendingOrder = (a, b) => {
         return a < b;
@@ -55,18 +85,20 @@ function ensureSortedInAscendingOrder(array) {
 
 function ensureSortedInDescendingOrder(array) {
   const comparer = (a, b) => {
-    return a => b;
+    return a >= b;
   };
   validate(array, comparer);
 }
 function validate(array, comparer) {
   const arrayLen = array.length;
+
   for (let index = 0; index < arrayLen; index++) {
     if (hasNextElem()) {
       const elem = array[index];
       const nextElem = array[index + 1];
       if (!comparer(elem, nextElem)) {
-        throw new Error("Array not sorted in ascending order.");
+        console.log(array, elem, nextElem);
+        throw new Error("Array not sorted in the required order.");
       }
     }
     function hasNextElem() {
